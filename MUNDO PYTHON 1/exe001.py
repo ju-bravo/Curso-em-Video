76368687fc73159d7ea445459fa5d5a59d49c91f
Juliana Bravo <ju-bravo@users.noreply.github.com>
2021-03-15 15:13:40 -0300

    Initial commit

---
+++ MUNDO PYTHON 1/exe001.py
@@ -0,0 +1,3 @@
+# exe001.py --> Crie um programa que escreva "olá, mundo!" na tela;
+
+print("olá, mundo!")
---
+++ MUNDO PYTHON 1/exe002.py
@@ -0,0 +1,5 @@
+#  exe002.py --> Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas;
+------------------------------------------------------------------------------------------------------|
+
+nome = input("Olá, qual é o seu nome?")
+print("É um prazer te conhecer {}!".format(nome))
